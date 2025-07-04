b1737bf79a2ffd72ebe0a495345fac81a93c2af6
matheus anjos <matheusdlanjos@hotmail.com>
2025-07-04 17:06:55 -0300

    commit

---
+++ string.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $age = 16;
+    print "You are ".$age." years old.<br>";
+    print "You are $age years old.<br>"; # You are 16 years old.
+    print ' You are $age years old.'; # You are $age years old.
+    ?>
+
+    <?php 
+    $cidade = "Curitiba";
+    $estado = "PR";
+    $idade = 325;
+    $frase_capital = "A cidade de $cidade é a capital do $estado";
+    $frase_idade = "$cidade tem mais de $idade anos";
+    echo "<h3>$frase_capital </h3>";
+    echo "<h4>$frase_idade </h4>"
+    ?>
+
+    <?php 
+    $age = 16;
+    print "Today is your $ageth birthday.<br>"; #ageth not found
+    print "Today is your {$age}th birthday."
+    
+    ?>
+    
+</body>
+</html>
