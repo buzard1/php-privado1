8c512f731cf1a39db1e4fbaf793b881ea1754c08
matheus anjos <matheusdlanjos@hotmail.com>
2025-07-11 15:33:44 -0300

    commit

---
+++ nota.php
@@ -0,0 +1,25 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Notas</title>
+    </head>
+    <body>
+        <?php 
+
+        $nota1 = 6;
+        $nota2 = 10;
+        $nota3 = 7;
+        $media = ($nota1+$nota2+$nota3) / 3;
+        
+        if($media >=7)
+        {
+            echo "Você está aprovado com media ".number_format($media, 1);
+        }else{
+            echo "Você ficou em exame com media ".number_format($media, 1);
+        }
+        ?>
+        
+    </body>
+    </html>
