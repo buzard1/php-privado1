f47ec628ef0952a15e60c7cb273fe99bb1aaebe7
matheus anjos <matheusdlanjos@hotmail.com>
2025-07-21 17:11:40 -0300

    c

--- session/banco.php
+++ session/banco.php
@@ -1,10 +1,10 @@
 <?php 
-    $bdServidor = 'localhost:8080';
+    $bdServidor = '127.0.0.1';
     $bdUsuario = 'root';
-    $bdSenha = '';
+    $bdSenha = '123';
     $bdBanco = 'matheus_anjos';
     $conexao = mysqli_connect($bdServidor,$bdUsuario,$bdSenha,$bdBanco);
-        if(mysqli_connect_errno($conexao)){
+        if(mysqli_connect_errno()){
             echo "Prolemas para conectar no banco. Verifique os dados";
             die();
         }
